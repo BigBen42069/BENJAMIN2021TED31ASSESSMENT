3c55479c8ef02fa14ed3e98f6a6fa6aa63898fb4
BigBen42069 <81542626+BigBen42069@users.noreply.github.com>
2021-05-03 14:33:55 +1200

    Add files via upload

---
+++ Assessment Quiz Program.py
@@ -0,0 +1,19 @@
+# Assessment Quiz Program
+# Benjamin Shortland
+
+"""
+Quiz Program lets go
+"""
+
+# import libraries
+from appJar import gui
+# variables
+question_db = []
+score = 0
+# class definitions
+class Gui:
+    pass
+
+
+# main routine
+Gui()
